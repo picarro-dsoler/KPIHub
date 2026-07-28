8b7f21039ecfbb749081df67174e25cf22fc82b3
picarro-dsoler <dsoler@picarro.com>
2026-07-28 18:43:23 +0000

    Updated the SurveySummaryIngester

--- lib/ingester/SurveySummaryIngester.py
+++ lib/ingester/SurveySummaryIngester.py
@@ -6,6 +6,9 @@ import os
 import sys
 import pandas as pd
 from datetime import datetime, date, timedelta
+import geopandas as gpd
+from shapely.geometry import Polygon, LineString, MultiLineString
+
 
 # Get the absolute path of the current file's directory
 directory = os.path.abspath(os.path.dirname(__file__))
@@ -22,7 +25,7 @@ from IngesterClass import Ingester
 from datetime import date
 from datetime import timedelta
 
-class SurveySummaryIngester(Ingester):
+class SurveySummaryIngesterDuplicates(Ingester):
     def __init__(self, arguments):
         super().__init__(arguments)
         self.table = KPI_SurveySummary
@@ -125,6 +128,208 @@ class SurveySummaryIngester(Ingester):
     def push_data(self):
         super().push_data(primary_key = ['SurveyId','ReportId'])
         self.Logger.info(f"Data pushed to {db_path}")
+
+
+#Updated class that handles the duplicates in Surveys on Report by working on the area table
+class SurveySummaryIngester(Ingester):
+    def __init__(self, arguments):
+        super().__init__(arguments)
+        self.table = KPI_SurveySummary
+
+    def update_check(self):
+        customer_name = self.customer_info['Name']
+        customer_id = self.customer_info['CustomerId']
+        customer_db = self.customer_info['DBLocation']
+
+        self.Logger.info(f"Processing customer: {customer_name}")
+        self.Logger.info(f"Getting reports from {self.update_window} to {self.current_date}")
+        #Query the last report
+        self.data['reports'] = Query(
+            f"""
+            SELECT ReportId, ReportDate, ReportArea, LastUpdated FROM KPI_ReportSummary
+            WHERE CustomerId = '{self.customer_info['CustomerId']}'
+            ORDER BY LastUpdated DESC
+            """
+        ).execute(KPIHub_Conn)
+
+        self.data['survey_count'] = Query(
+            f"""
+            SELECT COUNT(*) as SurveyCount
+            FROM KPI_SurveySummary
+            WHERE ReportId IN (SELECT ReportId FROM KPI_ReportSummary WHERE CustomerId = '{customer_id}')
+            """
+        ).execute(KPIHub_Conn)
+
+        if len(self.data['reports']) > 0:
+            if (self.data['survey_count'].iloc[0]['SurveyCount']) > 0:
+                self.check_flag = True
+                self.starting_date = self.update_window
+                self.Logger.info(f"Getting emissions from {self.update_window} to {self.current_date}")
+
+            else:
+                self.Logger.info(f"No emissions found, processing from start")
+                self.starting_date = STARTING_DATE
+                self.check_flag = True
+        else:
+            self.Logger.info(f"No reports found, skipping")
+            self.check_flag = False
+
+    def query_data(self):
+        if self.check_flag:
+            # Ensure the ReportDate values are in datetime format before comparison,
+            # handling both with and without microseconds (mixed formats)
+            self.data['reports']['ReportDate'] = pd.to_datetime(self.data['reports']['ReportDate'], format='mixed')
+
+            # Handle potential issues with type mismatch when comparing datetimes
+            # Coerce both sides to date for a robust comparison
+            #reports_to_query = self.data['reports'][
+            #    pd.to_datetime(self.data['reports']['ReportDate']).dt.date >= pd.to_datetime(self.starting_date).date()
+            #]
+            reports_to_query = self.data['reports'].copy()
+   
+            reports_to_query.db.set_query(query_surveys_table(report_table="#TempReports"))
+            surveys = reports_to_query.db.execute(CONN_DICT[self.customer_info['DBLocation']], source_col = 'ReportId', temp_table_name = '#TempReports')
+            surveys.db.set_query(query_segments_table(survey_table="#TempSurvey"))
+            self.Logger.info(f"Reports from KPI_ReportSummary: {len(reports_to_query)}")
+            self.Logger.info(f"Surveys from LSDB: {len(surveys)}")
+            segments = surveys.db.execute(CONN_DICT[self.customer_info['DBLocation']], source_col = 'SurveyId', temp_table_name = '#TempSurvey')
+            self.Logger.info(f"Segments from LSDB: {len(segments)}")
+
+
+            # Set the starting time as a datetime object
+            surveys['StartHour'] = pd.to_datetime(surveys['StartEpoch'], unit='s').dt.hour
+            surveys['StartTime'] = pd.to_datetime(surveys['StartEpoch'], unit='s').dt.time
+            surveys['EndHour'] = pd.to_datetime(surveys['EndEpoch'], unit='s').dt.hour
+            surveys['EndTime'] = pd.to_datetime(surveys['EndEpoch'], unit='s').dt.time
+            surveys['StartDay'] = pd.to_datetime(surveys['StartEpoch'], unit='s').dt.date
+            surveys['EndDay'] = pd.to_datetime(surveys['EndEpoch'], unit='s').dt.date
+
+            # Calculate the duration (in minutes) between StartEpoch and EndEpoch for each survey
+            surveys['DurationMinutes'] = (
+                surveys['EndEpoch'] - surveys['StartEpoch']
+            ) / 60      
+
+            # Prepare the report_gdf for report area lookup
+            reports_to_query["geometry"] = gpd.GeoSeries.from_wkt(reports_to_query["ReportArea"])
+
+            report_gdf = gpd.GeoDataFrame(
+                reports_to_query,
+                geometry="geometry",
+                crs="EPSG:4326"
+            )
+            utm_crs = report_gdf.estimate_utm_crs()
+            report_gdf = report_gdf.to_crs(utm_crs)
+
+            segments_gdf = gpd.GeoDataFrame(
+                segments,
+                geometry=gpd.GeoSeries.from_wkt(segments['Shape']),
+                crs="EPSG:4326"
+            )
+            segments_gdf = segments_gdf.to_crs(utm_crs)
+        
+            survey_summary = surveys.apply(survey_summary_apply, axis=1)
+            outputs =  []
+            self.data['reports_gdf'] = report_gdf
+            self.data['segments_gdf'] = segments_gdf
+            self.data['survey_gdf'] = surveys
+            for idx, row in report_gdf.iterrows():
+                self.Logger.File.info(f"Processing report {row['ReportId']}, {idx+1} of {len(report_gdf)}")
+                surveys_subset = surveys[surveys['ReportId'] == row['ReportId']]['SurveyId']
+                segments_subset = segments_gdf[segments_gdf['SurveyId'].isin(surveys_subset)]
+                segments_in_report = gpd.overlay(
+                    segments_subset,
+                    gpd.GeoDataFrame([row], geometry=[row['geometry']], crs=segments_subset.crs),
+                    how='intersection',
+                    keep_geom_type=False
+                )
+
+                if segments_in_report.empty:
+                    continue  # skip if intersection is empty
+                self.data['segments_in_report'] = segments_in_report
+                # Convert StartEpoch to datetime (time only, no date)
+                segments_in_report['StartTime'] = pd.to_datetime(segments_in_report['StartEpoch'], unit='s').dt.time
+                segments_in_report['StartDate'] = pd.to_datetime(segments_in_report['StartEpoch'], unit='s')
+                segments_in_report['DayNight'] = segments_in_report['StartTime'].apply(lambda t: get_day_night(t, SUNRISE_TIME, SUNSET_TIME))
+                segments_in_report['ActiveIdle'] = segments_in_report['CarSpeedMedian'].apply(lambda x: set_actie_idle(x, SPEED_THRESHOLD))
+
+                # Group by SurveyId and calculate the aggregated metrics per SurveyId (for surveys associated with this report)
+                segment_grouped = segments_in_report.groupby('SurveyId')
+
+                for survey_id, group in segment_grouped:
+                    output = {
+                        'SurveyId': survey_id,
+                        'ReportId': row['ReportId'],
+                        'DaySegments': (group['DayNight'] == 'Day').sum(),
+                        'NightSegments': (group['DayNight'] == 'Night').sum(),
+                        'ActiveSegments': (group['ActiveIdle'] == 'Active').sum(),
+                        'IdleSegments': (group['ActiveIdle'] == 'Idle').sum(),
+                        'TotalSegments': len(group),
+                        'TotalKilometers': group['LengthMeters'].sum() / 1000,
+                        'DayKilometers': group.loc[group['DayNight'] == 'Day', 'LengthMeters'].sum() / 1000,
+                        'NightKilometers': group.loc[group['DayNight'] == 'Night', 'LengthMeters'].sum() / 1000,
+                        'SegmentDurationMinutes': group['DurationSeconds'].sum() / 60,
+                        'IdleTimeMinutes': group.loc[group['ActiveIdle'] == 'Idle', 'DurationSeconds'].sum() / 60,
+                        'ActiveTimeMinutes': group.loc[group['ActiveIdle'] == 'Active', 'DurationSeconds'].sum() / 60,
+                        'AvgSpeedKm': 3.6 * group['CarSpeedMedian'].mean(),
+                    }
+                    outputs.append(output)
+       
+
+            output_df = pd.DataFrame(outputs)
+            merged_df = pd.merge(
+                survey_summary,
+                output_df,
+                left_on=["SurveyId", "ReportId"],
+                right_on=["SurveyId", "ReportId"],
+                how="inner"
+            )
+            merged_df['SegmentWeight'] = merged_df['TotalSegments'] / merged_df['TotalSegmentsInSurvey']
+            merged_df['SurveyDurationMinutes'] = merged_df['SurveyRawDurationMinutes'] * merged_df['SegmentWeight']
+            self.data['output'] = merged_df
+            self.data['output']['LastUpdated'] = datetime.now()
+        
+        
+    def sanity_check(self):
+        super().sanity_check()
+        #Get all the reports from the KPI_SurveySummary table
+        df_surveys = Query(query = f"SELECT DISTINCT ReportId FROM KPI_SurveySummary WHERE ReportId IN (SELECT ReportId FROM KPI_ReportSummary WHERE CustomerId = '{self.customer_info['CustomerId']}')").execute(KPIHub_Conn)
+        self.Logger.info(f"Total number of unique reports from KPI_SurveySummary: {len(df_surveys)}")
+
+    def push_data(self):
+        super().push_data(primary_key = ['SurveyId','ReportId'])
+        self.Logger.info(f"Data pushed to {db_path}")
+
+# Define a function to determine if survey is in 'day' or 'night'
+def get_day_night(start_time, sunrise, sunset):
+    # start_time should be a datetime.time object
+    hour = start_time.hour
+    if sunrise <= hour < sunset:
+        return 'Day'
+    else:
+        return 'Night'
+
+def set_actie_idle(speed, speed_threshold):
+    if speed < speed_threshold:
+        return 'Idle'
+    else:
+        return 'Active' 
+def survey_summary_apply(row):
+    return pd.Series({
+        'SurveyId': row['SurveyId'],
+        'SurveyorUnit': row['SurveyorUnit'],
+        'SurveyRawDurationMinutes': row['DurationMinutes'],
+        'ReportId': row['ReportId'],
+        'StartHour': row['StartHour'],
+        'StartTime': row['StartTime'],
+        'StartEpoch': row['StartEpoch'],
+        'EndTime': row['EndTime'],
+        'EndEpoch': row['EndEpoch'],
+        'StartDay': row['StartDay'],
+        'EndDay': row['EndDay'],
+        'LateralRotation': row['LateralRotation'],
+        'NumberOfPeaks': row['NumberOfPeaks'],
+        'TotalSegmentsInSurvey': row['TotalSegmentsInSurvey']
+    })
 
 # Define a function to determine if survey is in 'day' or 'night'
 def get_day_night(start_time, sunrise, sunset):
